8de98fb1a0a09254d21537faf6eff9af1415ed64
Carrillo-coder <A01567228@tec.mx>
2025-09-17 02:10:05 -0600

    Deleted extra stuff, created controller

--- web services/CRUDServer.py
+++ web services/CRUDServer.py
@@ -16,60 +16,21 @@ password = "YourPassword123!"
 def get_connection():
     return pymssql.connect(server=server, port=port, user=username, password=password, database=database)
 
-@app.route('/test', methods=['GET'])
-def get_all():
-    conn = get_connection()
-    cursor = conn.cursor(as_dict=True)
-    cursor.execute('SELECT * FROM Test')
-    data = cursor.fetchall()
-    conn.close()
-    return jsonify(data)
+@app.route('/get', methods=['GET'])
+def get():
+    return "Hello World", 200
 
-@app.route('/test/<int:id>', methods=['GET'])
-def get_one(id):
-    conn = get_connection()
-    cursor = conn.cursor(as_dict=True)
-    cursor.execute('SELECT * FROM Test WHERE id = %s', (id,))
-    data = cursor.fetchone()
-    conn.close()
-    if data:
-        return jsonify(data)
-    else:
-        return jsonify({'mensaje': 'Registro no encontrado'}), 404
+@app.route('/post', methods=['POST'])
+def post():
+    return "Item creado", 201
 
-@app.route('/test', methods=['POST'])
-def create():
-    data = request.json
-    id = data['id']
-    name = data['name']
-    email = data['email']
-    conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute('INSERT INTO Test (id, name, email) VALUES (%s,%s, %s)', (id, name, email))
-    conn.commit()
-    conn.close()
-    return jsonify({'mensaje': 'Registro creado'}), 201
+@app.route('/put/<int:id>', methods=['PUT'])
+def put(id):
+    return "Item actualizado: " + str(id), 200
 
-@app.route('/test/<int:id>', methods=['PUT'])
-def update(id):
-    data = request.json
-    name = data['name']
-    email = data['email']
-    conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute('UPDATE Test SET name = %s, email = %s WHERE id = %s', (name, email, id))
-    conn.commit()
-    conn.close()
-    return jsonify({'mensaje': 'Registro actualizado'})
-
-@app.route('/test/<int:id>', methods=['DELETE'])
+@app.route('/delete/<int:id>', methods=['DELETE'])
 def delete(id):
-    conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute('DELETE FROM Test WHERE id = %s', (id,))
-    conn.commit()
-    conn.close()
-    return jsonify({'mensaje': 'Registro borrado'})
+    return "Item eliminado: " + str(id), 200
 
 if __name__ == '__main__':
     app.run(debug=True, port=2025)
